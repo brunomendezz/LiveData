aca6714af3ce11705a58cde36fa26e4b8602951d
Bruno <brunomartinmendez@gmail.com>
2022-09-22 17:40:40 -0300

    Commit del ejercicio con liveData

--- app/src/main/java/ar/edu/unlam/ejerciciolivedata/view/MainActivity.kt
+++ app/src/main/java/ar/edu/unlam/ejerciciolivedata/view/MainActivity.kt
@@ -2,17 +2,15 @@ package ar.edu.unlam.ejerciciolivedata.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModel
-import androidx.viewbinding.ViewBinding
 import ar.edu.unlam.ejerciciolivedata.databinding.ActivityMainBinding
 import ar.edu.unlam.ejerciciolivedata.viewmodel.MainViewModel
-import kotlin.reflect.KProperty
+
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var vm: MainViewModel by viewModels()
+    private var vm: MainViewModel = MainViewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -26,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         })
 
         vm.booleanActual.observe(this, Observer {
-            var esparOno = it
+            val esparOno = it
             if(esparOno)
             {
                 binding.tvEsPar.text ="PAR"
@@ -36,18 +34,14 @@ class MainActivity : AppCompatActivity() {
         })
 
         binding.buttonMas.setOnClickListener{
-            vm.numeroActual.value=++vm.numero
-            vm.booleanActual.value=vm.numero%2==0
+            vm.numeroActual.postValue(++vm.numero)
+            vm.booleanActual.postValue(vm.numero%2==0)
            
         }
         binding.buttonMenos.setOnClickListener{
-            vm.numeroActual.value=--vm.numero
-            vm.booleanActual.value=vm.numero%2==0
+            vm.numeroActual.postValue(--vm.numero)
+            vm.booleanActual.postValue(vm.numero%2==0)
         }
 
     }
-}
-//ESTO LO HICE PORQ ME LO PEDIA EN EL by viewModels() ,no se que es..
-private operator fun Any.setValue(mainActivity: MainActivity, property: KProperty<*>, mainViewModel: MainViewModel) {
-
 }
